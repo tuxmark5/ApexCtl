e441a000e64ca6e61c0f42fec5cb9c94a5e3b6b3
Audrius <tuxmarkv@gmail.com>
2014-03-10 22:07:33 +0200

    code restructuring added command line options

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 import            Control.Concurrent
 import            Control.Exception
 import            Control.Monad
@@ -5,46 +7,66 @@ import            Data.Binary
 import            Data.ByteString (ByteString)
 import qualified  Data.ByteString as BS
 import qualified  Data.ByteString.Lazy as BL
+import            Data.Char (digitToInt)
 import qualified  Data.Vector as V
 import            Data.Word (Word16)
+import            System.Console.CmdArgs.Implicit
 import            System.USB
 import            Text.Printf
+
+{- ########################################################################################## -}
 
 _SET_REPORT :: Request
 _SET_REPORT = 0x09
 
-{-
-0040  16 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00   ........ ........
-0050  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00   ........ ........ 
-
-0040  07 00 0e f1 0e 08 ef fa  60 08 ef fa 60 08 ef fa   ........ `...`...
-0050  60 08 ef fa 60 08 00 00  00 00 00 00 00 00 00 00   `...`... ........
-0040  07 00 00 ff ff 08 00 ff  00 08 ff 00 ff 08 ff 00   ........ ........
-0050  00 08 ff ff ff 08 00 00  00 00 00 00 00 00 00 00   ........ ........
-
-07 00
-00 ff ff 08 
-00 ff 00 08 
-ff 00 ff 08 
-ff 00 00 08 
-ff ff ff 08 
-00 00 00 00 
-00 00 00 00 
-00 00
-
--}
-
 {- ########################################################################################## -}
 
-data Color = Color Word8 Word8 Word8 Word8
-
-data SetColorProfile = SetColorProfile
+data ApexCommand 
+  = CmdEnableExtraKeys
+  -- 0..8
+  | CmdSetBrightness Word8
+  -- 0 ->  125 Hz
+  -- 1 ->  250 Hz
+  -- 2 ->  500 Hz
+  -- 3 -> 1000 Hz
+  | CmdSetPollFrequency Word8
+  | CmdSetColorProfile
   { colorSouth  :: Color
   , colorEast   :: Color
   , colorNorth  :: Color
   , colorWest   :: Color
   , colorLogo   :: Color
-  }
+  } deriving (Show)
+
+instance Binary ApexCommand where
+  put (CmdEnableExtraKeys) = do
+    put32 [0x02, 0x00, 0x02]
+
+  put (CmdSetBrightness a) = do
+    put32 [0x05, 0x01, a]
+
+  put c@(CmdSetColorProfile {}) = do 
+    put (0x07 :: Word8)
+    put (0x00 :: Word8)
+    put $ colorSouth c
+    put $ colorEast c
+    put $ colorNorth c
+    put $ colorWest c
+    put $ colorLogo c
+    replicateM_ 10 $ put (0x00 :: Word8)
+
+  put (CmdSetPollFrequency rate) = do
+    put32 [0x04, 0x00, rate] 
+
+put32 :: [Word8] -> Put
+put32 words = do
+  forM_ words $ put
+  replicateM_ (32 - length words) $ put (0 :: Word8)
+
+{- ########################################################################################## -}
+
+data Color = Color Word8 Word8 Word8 Word8
+  deriving (Data, Show, Typeable)
 
 instance Binary Color where
   put (Color r g b a) = do
@@ -53,16 +75,36 @@ instance Binary Color where
     put b
     put a
 
-instance Binary SetColorProfile where
-  put cp = do
-    put (0x07 :: Word8)
-    put (0x00 :: Word8)
-    put $ colorSouth cp
-    put $ colorEast cp
-    put $ colorNorth cp
-    put $ colorWest cp
-    put $ colorLogo cp
-    replicateM_ 10 $ put (0x00 :: Word8)
+instance Read Color where
+  readsPrec _ = \str -> case str of
+    [r0, r1, g0, g1, b0, b1, ':', a0] ->
+      [(Color (toComp2 r0 r1) (toComp2 g0 g1) (toComp2 b0 b1) (toAlpha a0), [])]
+    [r0, r1, g0, g1, b0, b1] ->
+      [(Color (toComp2 r0 r1) (toComp2 g0 g1) (toComp2 b0 b1) 8, [])]
+    [r0, g0, b0, ':', a0] ->
+      [(Color (toComp1 r0) (toComp1 g0) (toComp1 b0) (toAlpha a0), [])]
+    [r0, g0, b0] ->
+      [(Color (toComp1 r0) (toComp1 g0) (toComp1 b0) 8, [])]
+
+toAlpha :: Char -> Word8
+toAlpha alpha = case digitToInt alpha of 
+  a | a >= 0 && a <= 8 
+    -> fromIntegral a 
+  otherwise 
+    -> error "brightness must be between 0 and 8" 
+
+toComp1 :: Char -> Word8
+toComp1 a = fromIntegral $ (digitToInt a) * 0x10 + 0xF
+
+toComp2 :: Char -> Char -> Word8
+toComp2 a b = fromIntegral $ (digitToInt a) * 0x10 + (digitToInt b)
+
+toFrequency :: Int -> Word8
+toFrequency  125 = 0
+toFrequency  250 = 1
+toFrequency  500 = 2
+toFrequency 1000 = 3
+toFrequency    _ = error "frequency must be 125, 250, 500 or 1000"
 
 {- ########################################################################################## -}
 
@@ -72,36 +114,97 @@ colorPurple = Color 0xFF 0x00 0xFF 0x08
 colorRed    = Color 0xFF 0x00 0x00 0x08
 colorWhite  = Color 0xFF 0xFF 0xFF 0x08
 
-cp0   = SetColorProfile colorRed colorRed colorRed colorRed colorRed
-cp1   = SetColorProfile colorCyan colorGreen colorPurple colorRed colorWhite
-cp2 c = SetColorProfile c c c c c
+cp0   = CmdSetColorProfile colorRed colorRed colorRed colorRed colorRed
+cp1   = CmdSetColorProfile colorCyan colorGreen colorPurple colorRed colorWhite
+cp2 c = CmdSetColorProfile c c c c c
 
 {- ########################################################################################## -}
 
-pad :: a -> Int -> [a] -> [a]
-pad p 0 l     = l
-pad p n (h:l) = h:(pad p (n-1) l)
-pad p n []    = pad p n [p]
+data ApexMode
+  = ModeEnableExtraKeys
+  | ModeSetBrightness 
+  { argBrightness :: Word8 }
+  | ModeSetColorProfile
+  { argSouth  :: String
+  , argEast   :: String
+  , argNorth  :: String
+  , argWest   :: String
+  , argLogo   :: String
+  } 
+  | ModeSetPollFrequency 
+  { argFrequency :: Int }
+  deriving (Data, Show, Typeable)
 
-{- ########################################################################################## -}
+apexArgs :: ApexMode
+apexArgs = modes 
+  [ modeEnableExtraKeys
+  , modeSetBrightness
+  , modeSetColorProfile
+  , modeSetPollFrequency
+  ] 
+  &= program "apexctl" 
+  &= summary "An utility for managing Apex/Apex [RAW] keyboard settings"
 
-cdataInit :: BS.ByteString
-cdataInit = BS.pack $ pad 0x00 32 [ 0x02, 0x00, 0x02 ]
+modeEnableExtraKeys :: ApexMode
+modeEnableExtraKeys = ModeEnableExtraKeys 
+  &= auto 
+  &= help "Enable macro keys"
+  &= name "init"
 
-cdata :: BS.ByteString
-cdata     = BS.pack $ pad 0x00 32 [ 0x16 ]
+modeSetBrightness :: ApexMode 
+modeSetBrightness = ModeSetBrightness
+  { argBrightness = 8 &= argPos 0 &= typ "BRIGHTNESS"
+  }
+  &= help "Set brightness level (1..8)"
+  &= name "br"
 
--- rate in [0..3]
--- 0 ->  125 Hz
--- 1 ->  250 Hz
--- 2 ->  500 Hz
--- 3 -> 1000 Hz
-pollFrequency :: Word8 -> BS.ByteString
-pollFrequency rate = BS.pack $ pad 0x00 32 [ 0x04, 0x00, rate ]
+colorAnnot :: String -> String -> String -> String
+{-# INLINE colorAnnot #-}
+colorAnnot name0 name1 desc = "FF0000:8" 
+  &= explicit 
+  &= name name0 
+  &= name name1 
+  &= help desc
+  &= typ "COLOR"
 
--- bLev in [1..8]
-brightness :: Word8 -> BS.ByteString
-brightness bLev    = BS.pack $ pad 0x00 32 [ 0x05, 0x01, bLev ]
+modeSetColorProfile :: ApexMode
+modeSetColorProfile = ModeSetColorProfile
+  { argSouth = colorAnnot "s" "south" "Color for South zone"
+  , argEast  = colorAnnot "e" "east"  "Color for East zone"
+  , argNorth = colorAnnot "n" "north" "Color for North zone"
+  , argWest  = colorAnnot "w" "west"  "Color for West zone"
+  , argLogo  = colorAnnot "l" "logo"  "Color for Logo zone"
+  } 
+  &= help (unlines 
+    [ "Change color profile."
+    , "Colors should be specified for all color zones."
+    , "Otherwise unspecified colors will default to red" ])
+  &= name "colors" 
+
+modeSetPollFrequency :: ApexMode
+modeSetPollFrequency = ModeSetPollFrequency
+  { argFrequency = 1000 
+    &= argPos 0 
+    &= typ "FREQUENCY" 
+  }
+  &= help "Set polling frequency (125, 250, 500 or 1000)"
+  &= name "freq" 
+
+modeToCommand :: ApexMode -> ApexCommand
+
+modeToCommand m@ModeEnableExtraKeys {} = CmdEnableExtraKeys
+
+modeToCommand m@ModeSetBrightness {} = CmdSetBrightness (argBrightness m)
+
+modeToCommand m@ModeSetColorProfile {} = CmdSetColorProfile
+  { colorSouth  = read $ argSouth m
+  , colorEast   = read $ argEast m
+  , colorNorth  = read $ argNorth m
+  , colorWest   = read $ argWest m
+  , colorLogo   = read $ argLogo m
+  }
+
+modeToCommand m@ModeSetPollFrequency {} = CmdSetPollFrequency (toFrequency $ argFrequency m)
 
 {- ########################################################################################## -}
 
@@ -116,6 +219,11 @@ apexCtl devHndl d = do
     0
     d
     noTimeout
+
+apexCommand :: Binary b => DeviceHandle -> b -> IO ()
+apexCommand devHndl b = do
+  apexCtl devHndl $ toStrict1 $ encode b
+  threadDelay 10000
 
 apexRet :: DeviceHandle -> IO ()
 apexRet devHndl = do
@@ -153,35 +261,15 @@ withDevice venId prodIds hnd = do
 
 {- ########################################################################################## -}
 
-apexEnableExtraKeys :: DeviceHandle -> IO ()
-apexEnableExtraKeys devHndl = apexCtl devHndl cdataInit
-
-apexSetColorProfile :: DeviceHandle -> SetColorProfile -> IO ()
-apexSetColorProfile devHndl cp = do
-  apexCtl devHndl $ toStrict1 $ encode cp
-  threadDelay 10000
-
-apexRawSetBrightness :: DeviceHandle -> Word8 -> IO ()
-apexRawSetBrightness devHndl bLev = do
-  apexCtl devHndl $ brightness bLev
-
-apexSetFrequency :: DeviceHandle -> Word8 -> IO ()
-apexSetFrequency devHndl rate = do
-  apexCtl devHndl $ pollFrequency rate
-
-{- ########################################################################################## -}
-
 main :: IO ()
 main = do
-  withDevice 0x1038 [0x1200,0x1202] $ \dev -> do
+  args <- cmdArgs apexArgs
+  withDevice 0x1038 [0x1200, 0x1202] $ \dev -> do
   withDeviceHandle dev $ \devHndl ->
     withDetachedKernelDriver devHndl 0 $
     withClaimedInterface devHndl 0 $ do
       res <- try $ do
-        apexEnableExtraKeys devHndl
-        --apexSetColorProfile devHndl cp0
-        --apexSetFrequency devHndl 3
-        --apexRawSetBrightness devHndl 8
+        apexCommand devHndl $ modeToCommand args
       case res of
         Left (SomeException a)  -> putStrLn $ show a
         Right _                 -> putStrLn "OK"
